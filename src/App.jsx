9947bcd66c7586cde1ad5d550cca66607d5b89d7
alik-balika <balikaalik@gmail.com>
2024-02-15 16:34:30 -0800

    complete first user story allowing user to change background color from input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [backgroundColor, setBackgroundColor] = useState("#FFFFFF");
+
+  const handleUserInput = (e) => {
+    if (e.target.value.length > 7) return;
+
+    console.log(e.target.value);
+    console.log(backgroundColor);
+
+    setBackgroundColor(e.target.value.toUpperCase());
+  };
+
+  return (
+    <div style={{ height: "100vh", backgroundColor: backgroundColor }}>
+      <div className="header">
+        <h1>React Color Cycle App</h1>
+      </div>
+      <div className="options">
+        <label htmlFor="colorCode">Color Code: </label>
+        <input
+          type="text"
+          id="colorCode"
+          value={backgroundColor}
+          onChange={handleUserInput}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
